bde7d78287c3cbad403b6de369f145fdc55a2673
Stephan93uh <stephan9@hawaii.edu>
2022-09-29 00:28:46 -1000

    Completed parts 1-7

---
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//Part 3
+var fruitNames: [String]
+
+//Part 4
+fruitNames = ["Kiwi", "Apples", "Watermelon"]
+
+//Part 5
+print(fruitNames)
+
+//Part 6
+print("I like to eat \(fruitNames)")
+
+//Part 7
+for fruit in fruitNames {
+    print("I like to eat " + fruit)
+}
+
+//Part 8
+print(fruitNames[1])
