6d4f0a7421fe3933f9979accb7d9d69d16d34684
Stephan93uh <stephan9@hawaii.edu>
2022-09-29 00:38:38 -1000

    Completed Part 9

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -21,3 +21,12 @@ for fruit in fruitNames {
 
 //Part 8
 print(fruitNames[1])
+
+//Part 9
+var expensiveCars: [String]
+
+expensiveCars = ["Rolls-Roice Boat Tail", "Bugatti la Voiture Noire", "Pagani Zonda HP Barchetta", "Rolls-Royce Sweptail", "Bugatti Centodieci", "Lamborghini Veneno", "Maybach Exelero", "Bugatti Divo"]
+
+for car in expensiveCars {
+    print("The " + car + " is a very expensive car.")
+}
